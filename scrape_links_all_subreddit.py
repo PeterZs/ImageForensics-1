f1d1a3519f0890a33fbe8d101d55141fafad47ad
Mark Heimann <mark.heimann@yahoo.com>
2016-04-05 15:25:26 -0400

    some exception handling

--- scrape_links_all_subreddit.py
+++ scrape_links_all_subreddit.py
@@ -27,57 +27,59 @@ def scrape():
   while img_count < NUM_LINKS_TO_GET: #TODO need this?
 
     for submission in submissions: #go through each submission
-      if img_count > NUM_LINKS_TO_GET:
-        break
-      print "next submission: ", submission
-      if not submission:
-        print "Not a submission"
-      else:
-        print "submission id: ", submission.id
-        #decide if images from this submission will be for training or test
-        link_file = None
-        testing = False #are we adding images to testing or training
-
-        #add first images to test (avoid divide by 0)
-        #then add to test if we have more than 5x number of training images as testing
-        #NOTE: add all images from a submission to train or test
-        #otherwise classifier might have very similar images in training dataset
-        if img_count_test == 0 or img_count_train / img_count_test >= 5: 
-          testing = True
-          link_file = link_file_test
+      try:
+        if img_count > NUM_LINKS_TO_GET:
+          break
+        print "next submission: ", submission
+        if not submission:
+          print "Not a submission"
         else:
-          link_file = link_file_train
-        #get all comments for now (note: may cause dataset imbalance?
-        #also takes longer because more API calls)
-        submission.replace_more_comments(limit=NUM_IMGS_PER_COMMENT, threshold = 0) #limit=None for all comments
-        comments = [comment for comment in submission.comments if \
-        not isinstance(comment, praw.objects.MoreComments) and comment.is_root][:NUM_IMGS_PER_COMMENT] #look for at most 10 images a comment
+          print "submission id: ", submission.id
+          #decide if images from this submission will be for training or test
+          link_file = None
+          testing = False #are we adding images to testing or training
 
-        count_comments = 1
-        for comment in comments: #each (root) comment (containing image)
-          #if we've made it this far assume image is original
-          links = find_links(comment.body) #get links (presumably of images)
+          #add first images to test (avoid divide by 0)
+          #then add to test if we have more than 5x number of training images as testing
+          #NOTE: add all images from a submission to train or test
+          #otherwise classifier might have very similar images in training dataset
+          if img_count_test == 0 or img_count_train / img_count_test >= 5: 
+            testing = True
+            link_file = link_file_test
+          else:
+            link_file = link_file_train
+          #get all comments for now (note: may cause dataset imbalance?
+          #also takes longer because more API calls)
+          submission.replace_more_comments(limit=None, threshold = 0) #limit=None for all comments
+          comments = [comment for comment in submission.comments if \
+          not isinstance(comment, praw.objects.MoreComments) and comment.is_root][:NUM_IMGS_PER_COMMENT] #look for at most 10 images a comment
 
-          #this link is valid so download image at this link
-          for link in links:
-            if "](" in link: #correct mistake made by regex: sometimes get http://imgur.com...](http://imgur.com...)
-              link = link[:link.index("]")] #get only up to and not including the ]
-            link = link.replace(")","") #because sometimes Imgur links have trailing )
-            link_file.write(link + " ")
-            link_file.write(submission.id + " ")
-            link_file.write(str(count_comments) + " ")
-            link_file.write(str(img_count + 1) + "\n")
-            img_count += 1
-            if testing:
-              img_count_test += 1
-            else:
-              img_count_train += 1
+          count_comments = 1
+          for comment in comments: #each (root) comment (containing image)
+            #if we've made it this far assume image is original
+            links = find_links(comment.body) #get links (presumably of images)
 
-          count_comments += 1 #count comment
-        count_submissions += 1 #count this submission as a new one
-        print("%d valid comments on submission %d. Now %d image links total: %d train, %d test" \
-          % (count_comments - 1, count_submissions - 1, img_count, img_count_train, img_count_test))
+            #this link is valid so download image at this link
+            for link in links:
+              if "](" in link: #correct mistake made by regex: sometimes get http://imgur.com...](http://imgur.com...)
+                link = link[:link.index("]")] #get only up to and not including the ]
+              link = link.replace(")","") #because sometimes Imgur links have trailing )
+              link_file.write(link + " ")
+              link_file.write(submission.id + " ")
+              link_file.write(str(count_comments) + " ")
+              link_file.write(str(img_count + 1) + "\n")
+              img_count += 1
+              if testing:
+                img_count_test += 1
+              else:
+                img_count_train += 1
 
+            count_comments += 1 #count comment
+          count_submissions += 1 #count this submission as a new one
+          print("%d valid comments on submission %d. Now %d image links total: %d train, %d test" \
+            % (count_comments - 1, count_submissions - 1, img_count, img_count_train, img_count_test))
+      except Exception as e:
+        print "exception: ", e
     break
 
   #finish up
