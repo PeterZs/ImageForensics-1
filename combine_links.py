fbe0a841059e3104a5c881c2c7ed8506a9ce10f5
Mark Heimann <mark.heimann@yahoo.com>
2016-04-06 22:44:14 -0400

    so we can scrape images from links

---
+++ combine_links.py
@@ -0,0 +1,78 @@
+#Combine training or test links from multiple scraping batches
+#Also allows us to check that there is no overlap between train and test data
+#  and correct if there is
+
+import os, pickle
+
+#Input: name of a file of all links we want to create or add to
+# name of file containing links to add
+#Action: add link info to file of all links 
+# (goal: avoid duplicate links, get just link itself and not
+#other info that the scraper collects just in case)
+def combine_links(links_file_name, links_to_add_file_name):
+  links = set()
+
+  #if we already have a list of all links so far
+  if os.path.exists(links_file_name):
+    #start by reading them in
+    links = pickle.load(open(links_file_name,"r"))
+
+  print("before reading in new links: %d links total" % len(links))
+
+  #add links from our file
+  links_to_add_file_name
+  links = read_in_links(links, links_to_add_file_name)
+
+  print("after reading in new links: %d links total" % len(links))
+
+  #save links
+  pickle.dump(links, open(links_file_name, "w"))
+
+#Input: a set of links and a name of file of links to add in
+#Output: set of links with links in file added
+def read_in_links(links, links_file_name):
+  links_file = open(links_file_name, "r")
+  links_info = links_file.readlines()
+  for link in links_info:
+    link_info = link.split() #info separated by split
+    links.add(link_info[0]) #first part is actual link
+  return links
+
+#Input: train and test link files
+#Output: boolean determining whether there is any overlap
+def check_dataset_integrity(train_file_name, test_file_name):
+  train_links = pickle.load(open(train_file_name, "r"))
+  test_links = pickle.load(open(test_file_name, "r"))
+  print("%d train, %d test links" % (len(train_links), len(test_links)))
+  intersection = set.intersection(train_links, test_links)
+  if len(intersection) > 0:
+    #hopefully this won't happen but if it does we need to 
+    #discard duplicate links from at least one dataset
+    print("%d links in both datasets" % len(intersection))
+
+    #Remove duplicate links from test data
+    print("Removing duplicates from test data...")
+    test_links_no_duplicates = test_links - intersection
+    pickle.dump(test_links_no_duplicates, open(test_file_name, "w"))
+    print("Pruned test data links saved")
+    print("New dataset sizes: ")
+    print("Train data: " % len(train_links))
+    print("Test data: " % len(test_links_no_duplicates))
+    return False 
+  else:
+    return True
+
+
+if __name__ == "__main__":
+  #which mode to run program in
+  read_data = True
+  check_data = False
+
+  if read_data: #we want to read data
+    links_file_name = "small_example_links" #"train_links"
+    links_to_add_file_name = "links-small-example.txt" #"links-topyear-all-train.txt"
+    combine_links(links_file_name, links_to_add_file_name)
+  if check_data: #we want to check integrity of our datasets
+    train_links_file_name = "train_links"
+    test_links_file_name = "test_links"
+    print check_dataset_integrity(train_links_file_name, test_links_file_name)
---
+++ get_images_from_links.py
@@ -0,0 +1,121 @@
+#Given file of image links, get images from them
+#Multithreading tutorial from http://www.codingninja.co.uk/building-a-concurrent-web-scraper-with-python/
+import urllib2, urllib, pickle, os #TODO get rid of one of these url imports
+from joblib import Parallel, delayed
+import multiprocessing
+from threading import Thread
+from uuid import uuid4
+from PIL import Image
+
+#Input: name of file of links to get images from
+# name of directory to save images to
+def get_images_from_links(links_file_name, img_dir_name):
+  #read in set of links as list (for parallelizing)
+  links = list(pickle.load(open(links_file_name, "r")))
+  #print "last link: ", links[-1]
+  links = links[:50]
+  num_cores = multiprocessing.cpu_count()
+  print "number of cores: ", num_cores
+
+  num_links_originally = len(links)
+  print("%d links" % num_links_originally)
+  #read links in parallel
+  Parallel(n_jobs=num_cores)(delayed(save_image_from_link)(link, img_dir_name) for link in links)
+  '''
+  for link_index in range(len(links)):
+    if link_index % 50 == 0:
+      print "link index ", link_index
+    link = links[link_index]
+    save_image_from_link(link,img_directory)
+    #Thread(target=save_image_from_link, args=(link,img_directory)).start()
+  '''
+
+  #remove any files that are not images
+  print "saved images"
+
+  files = os.listdir(img_dir_name)
+  Parallel(n_jobs=num_cores)(delayed(remove_if_not_image)(img_dir_name, img_file) for img_file in files)
+  '''
+  for img_file in os.listdir(img_dir_name):
+    try:
+      image = Image.open(img_file)
+    except IOError: #file not image, so remove it
+      num_non_images += 1
+      os.remove(img_dir_name + "/" + img_file)
+  '''
+  #print("removed %d non-image files" % num_non_images)
+  print("%d images saved from %d links" % (len(os.listdir(img_dir_name)), num_links_originally))
+
+#Input: image filename, directory where file is located
+#Action: remove image file if it is not an image
+def remove_if_not_image(img_dir_name, img_file):
+  try:
+    image = Image.open(img_dir_name + "/" + img_file)
+  except IOError: #file not image, so remove it
+    os.remove(img_dir_name + "/" + img_file)
+
+#Make HEAD requests without transferring entire file
+class HeadRequest(urllib2.Request):
+    def get_method(self):
+        return 'HEAD'
+
+#Input: link to save image from
+# directory to save image to
+#Action: save image to that link if valid image at that link
+def save_image_from_link(link, image_dir_name):
+  valid_image = True#, maintype = check_image_link_validity(link)
+
+  if link.endswith(".gifv"): #sometimes these throw us off
+    link = link[:-1] #remove trailing v
+  if link.startswith("http://imgur.com"):
+    link += ".jpg"
+
+  #last check: sometimes imgur links are to webpage not image
+  '''
+  if not valid_image and maintype == "text/html" and "imgur.com/" in link:
+    print "original link: ", link
+    if link.endswith(".gifv"): #sometimes these throw us off
+      link = link[:-1] #remove trailing v
+    else:
+      link += ".jpg" #extension doesn't matter but this redirects to image page
+    valid_image, maintype = check_image_link_validity(link) #redo check with new link
+  '''
+
+
+  if valid_image: #save the image
+    try:
+      extension = "jpg" #maintype.split("/")[-1] 
+      if extension == "html":
+        print "Link for html \"image\": ", link
+        #print "maintype: ", maintype
+        #give them any name (uuid)
+      urllib.urlretrieve(link, image_dir_name + "/" + str(uuid4()) + "." + extension)
+    except Exception as e:
+      print "Exception ", e
+      print "caused by link ", link
+  return valid_image
+
+#Input: link
+#Output: boolean True if link is for valid image, False otherwise
+# and request type
+def check_image_link_validity(link):
+  print "link being checked: ", link
+  valid_image = True
+  maintype = None
+  try:
+    response = urllib2.urlopen(HeadRequest(link))
+    maintype = response.headers['Content-Type'].split(';')[0].lower()
+    print "Resource type: ", maintype
+    #not valid image type
+    if not maintype.startswith("image/"):
+      #logging.debug('invalid type')
+      valid_image = False
+  except Exception as e:
+    print "Exception ", e
+    print "Link: ", link
+  return valid_image, maintype
+
+if __name__ == "__main__":
+  links_file_name = "small_example_links" #"test_links"
+  img_directory = "example_images_small"# "test-images"
+  get_images_from_links(links_file_name, img_directory)
